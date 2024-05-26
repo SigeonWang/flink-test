07f3f64cfd41b30fff01a243d7add3f3f5d250b1
兴建 王 <442135537@@qq.com>
2024-05-26 15:41:47 +0800

    Initial commit

---
+++ src/main/scala/com/wxj/batch/WordCount.scala
@@ -0,0 +1,31 @@
+package com.wxj.batch
+
+import org.apache.flink.api.scala._
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+
+/**
+ *
+ * 注意：
+ *    DataSet API的环境上下文是ExecutionEnvironment、分组函数是groupBy();
+ *    flink1.12开始DataSet API已被逐渐弃用，官方建议使用Table API / SQL执行批处理，或者使用带有批处理的DataStream API.
+ *
+ */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    // 1、创建环境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // 2、读取数据源
+    val linesDS = env.readTextFile("src/main/resources/word.txt")
+
+    // 3、切分、转换、分组、聚合
+    val wordsDS = linesDS.flatMap(_.split(" "))
+    val tupleDS = wordsDS.map((_, 1))
+    val tupleKS = tupleDS.groupBy(0)
+    val resTupleDS = tupleKS.sum(1)
+
+    // 4、输出
+    resTupleDS.print()
+  }
+}
---
+++ src/main/scala/com/wxj/stream/SocketWordCount.scala
@@ -0,0 +1,50 @@
+package com.wxj.stream
+
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+
+/**
+ *
+ * 注意：
+ *    DataStream API的环境上下文是StreamExecutionEnvironment、分组函数是keyBy();
+ *    DataStream API 最后需要调用execute()方法触发执行.
+ *
+ */
+object SocketWordCount {
+  def main(args: Array[String]): Unit = {
+    // TODO 1、创建环境
+    // 带web ui的本地测试环境，一般只用于测试。需要引入以来flink-runtime-web。
+     val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration())
+    // 推荐做法，自动推测运行环境
+//    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // 全局并行度设置：算子 > env > 提交命令 > 配置文件
+    //    算子关系：一对一；重分区
+    //    注意：在开发环境中，由于没有flink配置文件，默认并行度就是当前机器的CPU核心数
+    env.setParallelism(1)
+
+    // 全局禁用算子链（一般不需要禁用，自动优化即可。特殊：算子复杂度较高时；为了定位具体算子问题时）
+    //    算子链条件：一对一关系 + 并行度相同
+    //    也可以针对某个算子进行禁用，其前后都不会加入算子链：flatMap().disableChaining()
+    //    也可以针对某个算子开启后续的算子链，flatMap().startNewChain()
+//    env.disableOperatorChaining()
+
+    // TODO 2、读取数据源
+    val linesDS = env.socketTextStream("localhost", 6666)
+
+    // TODO 3、切分、转换、分组、聚合
+    val wordsDS = linesDS.flatMap(_.split(" "))
+    // 注意：一般不会在程序中设置全局并行度，因为如果在程序中对全局并行度进行硬编码，会导致无法动态扩容
+    val tupleDS = wordsDS.map((_, 1)).setParallelism(2)
+    // 注意：keyBy是个hash操作，不是算子，所以无法对keyBy设置并行度
+    val tupleKS = tupleDS.keyBy(_._1)
+    val resTupleDS = tupleKS.sum(1)
+
+    // TODO 4、输出
+    resTupleDS.print()
+
+    // TODO 5、触发执行
+    env.execute()
+  }
+}
